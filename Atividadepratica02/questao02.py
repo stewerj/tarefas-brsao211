8e6057be8c75c69fd1a6be9c0816286dac182727
stewerj <stewerj@gmail.com>
2026-01-13 17:31:49 -0300

    atividade pratica 01 - questão 01

--- Atividadepratica02/questao02.py
+++ Atividadepratica02/questao02.py
@@ -7,7 +7,6 @@ valor_desconto = valor * (desconto / 100)
 valor_final = valor - valor_desconto
 
 print(f"Produto: {nome_produto}")
-
 print(f"Preço Unitário: R${valor:.2f}")
 print(f"Desconto: R${valor_desconto:.2f}")
-print(f"Valor final: R${valor_final:.2f}")
+print(f"Preço total: R${valor_final:.2f}")
--- Atividadepratica02/questao04.py
+++ Atividadepratica02/questao04.py
@@ -5,4 +5,6 @@ combustivel_gasto = float(input("Digite o combustível gasto (em litros): "))
 consumo_medio = distancia_percorrida / combustivel_gasto
 
 
+print(f"Distância percorrida: {distancia_percorrida:.2f} km")
+print(f"Combustível gasto: {combustivel_gasto:.2f} litros")
 print(f"Consumo médio: {consumo_medio:.2f} km/l")
