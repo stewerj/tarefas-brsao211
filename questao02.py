0e25f679ba6eee17c2abdcbb1f3927288bbb86c6
stewerj <stewerj@gmail.com>
2026-01-13 08:29:10 -0300

    atividade 01 questão 02

--- questao02.py
+++ questao02.py
@@ -4,3 +4,7 @@ numero2 = 14
 soma = numero1 + numero2
 
 print("A soma dos números é:", soma)
+
+print("A soma dos numeros", numero1, "e", numero2, "é: ", soma)
+
+print(f"A soma dos números {numero1} e {numero2} é: {soma}")
