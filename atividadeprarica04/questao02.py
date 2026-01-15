8007c203c39c19adfcd4748addd43a77af992421
stewerj <stewerj@gmail.com>
2026-01-15 09:39:56 -0300

    atividade pratica 04  - questao 02

--- atividadeprarica04/questao02.py
+++ atividadeprarica04/questao02.py
@@ -1,26 +1,31 @@
 notas = []
 
 while True:
-    try:
-        entrada = input("Digite uma nota entre 0 e 10 (ou 'fim'para sair): ")
-        nota = float(input("Digite uma nota entre 0 e 10 (ou -1 para finalizar): "))
+   try:
+      entrada = input("Digite uma nota entre 0 e 10 (ou 'fim' para sair): ")
 
-        if entrada.lower() == 'fim':
-            break
-        nota = float(entrada)
+      if entrada.lower() == 'fim':
+         break
+      
+      nota = float(entrada)
 
-        if nota == < 0 or nota > 10':
-            raise Exception()
-        nota.append(nota)
+      if nota < 0 or nota > 10:
+         raise Exception ()
+      
+      notas.append(nota)
+
+
 
     except ValueError:
-        print("Você deve digitar apenas números.")
+        print("Você deve deve digitar apenas números.")
     except Exception:
-        print("Nota inválida.")
-        
+        print("Nota invalida.")
+
 soma = 0
 
 for nota in notas:
     soma += nota
-media = soma / len(notas) 
-print(f"Média final: {media:.2f}")
+
+média = soma / len(notas)
+
+print(f"Média final: {média:.2f}")
