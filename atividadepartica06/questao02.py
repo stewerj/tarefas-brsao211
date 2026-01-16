58c472074823431947b5042b18d95053eed1a69c
stewerj <stewerj@gmail.com>
2026-01-16 20:29:59 -0300

    atividade pratica 06 - questão 02

---
+++ atividadepartica06/questao02.py
@@ -0,0 +1,20 @@
+import requests
+
+def obter_ususario_aleatorio():
+    url = "https://randomuser.me/api/"
+    try:
+        requests = requests.get(url)
+        dados = requests.json() {"results"}[0]
+
+        nome = f" {dados {"name"}{"first"}{dados{"name"}{"last"}"}
+        email = dados{"email"}
+        pais = dados{"location"}{"country"}
+        sobrenome = dados{"name"}{"last"}
+
+        return f"nome: {nome}, email: {email}, pais: {pais}, "
+
+    except requests.exceptions.RequestException as e:
+        return f"Erro ao obter usuário aleatorio: {e}"   
+
+usuario = obter_ususario_aleatorio()
+print(usuario) 
