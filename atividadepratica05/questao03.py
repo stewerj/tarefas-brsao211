a2d25f92336642e5fee58ffdaf6b9167bb542b7c
stewerj <stewerj@gmail.com>
2026-01-20 06:21:02 -0300

    atividade pratica 05 - questão 03

---
+++ atividadepratica05/questao03.py
@@ -0,0 +1,14 @@
+def calcular_desconto (valor_produto, porcentagem_desconto):
+    desconto = valor_produto * (porcentagem_desconto / 100)
+    valor_final = valor_produto - desconto
+    return valor_final
+
+    
+    
+
+valor = float (input ("Digite o valor do produto: " )) 
+porcentagem = float (input ("Digite a porcentagem de desconto: " ))
+
+desconto = calcular_desconto(valor, porcentagem)
+
+print (f"O valor com desconto é : R$ {desconto:.2f}")
