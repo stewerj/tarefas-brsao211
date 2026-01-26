581e9583710d95bb8c66dc6bb2ca3301f33bc2f3
stewerj <stewerj@gmail.com>
2026-01-26 10:46:13 -0300

    logs treianemnto

---
+++ atividadepratica07/questao01.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def processar_logs_treinamento(arquivo_log):
+    try:
+        leitor= pd.read_csv(arquivo_log)
+        media = leitor['tempo_execucao'].mean()
+        desvio_padrao = leitor['tempo_execucao'].std()
+        return  f"Media: {media}, Desvio_Padrâo: {desvio_padrao}"
+                                
+        
+    except FileNotFoundError:
+        return "Arquivo não encontardo"
+    
+arquivo = input ("Digite o nome do arquivo de log:")    
+print (processar_logs_treinamento(arquivo))
