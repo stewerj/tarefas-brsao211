4bbc3508d1259f03bdc862e36b15a2cfaf72c02e
stewerj <stewerj@gmail.com>
2026-01-26 09:31:27 -0300

    atividade pratica 04  - questão 02

--- atividadepratica04/questao02.py
+++ atividadepratica04/questao02.py
@@ -14,16 +14,25 @@ while True:
       
       notas.append(nota)
 
+      soma = 0
+
+      for nota in notas:
+         soma += nota 
+      
+
+      media = soma / len(notas)   
+      print(len(notas))
+
+
+      print(f"Média final: {media:.2f}")
+
+
+
    except ValueError:
       print("Você deve digitar apenas números.")
 
    except Exception:
-      print("Nota inválida.")   
+      print("Nota inválida.") 
 
-soma = 0
-for nota in notas:
-   soma += nota 
-
-media = soma / len(notas)   
-
-print(f"Média final: {media:.2f}")
+   except ZeroDivisionError:     
+      print("Você deve digitar pelo menos uma nota.")
