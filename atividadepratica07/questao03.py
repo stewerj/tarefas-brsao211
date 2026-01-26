755ff01c6c8dc4f22e42156ca8e7ae0a0f7fe0c8
stewerj <stewerj@gmail.com>
2026-01-26 16:11:18 -0300

    atividade pratica 07 - questão 03

--- atividadepratica07/questao03.py
+++ atividadepratica07/questao03.py
@@ -1,14 +1,16 @@
 import csv
 
-def ler_arquivo_csv(nome_arquivo):
+def ler_arquivo(nome_arquivo):
     try:
-        with open(nome_arquivo, 'r' newline ="" , encoding = 'utf+8') as arquivo_csv
-        leitor= csv.reader(arquivo_csv)
-        for linha in leitor:
-            print(linha)
+        with open(nome_arquivo, 'r', newline="", encoding = 'utf-8') as arquivo_csv:
+            leitor = csv.reader(arquivo_csv)
+            for linha in leitor:
+                print(linha)
     except FileNotFoundError:
         print(f"Arquivo {nome_arquivo} não  encontrado.")
 
-nome_arquivo = 'dados.csv'
-ler_arquivo_csv(nome_arquivo) as arquivo_csv:
+
+
+nome_arquivo = input("Digite o nome do arquivo CSV: ")
+ler_arquivo(nome_arquivo)
                   
